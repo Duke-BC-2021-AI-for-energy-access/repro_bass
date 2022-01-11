e397dc9964ed80bbcf95d48641d6b858b2fe5634
Frank Willard <fcw@pizer.egr.duke.edu>
2022-01-11 14:01:54 -0500

    Updated shapes maker

--- scripts/extraneous/shapes_file_maker.py
+++ scripts/extraneous/shapes_file_maker.py
@@ -1,11 +1,15 @@
 import glob
 
-baseline_files = glob.glob("/scratch/public/txt_files/MW_baseline/*.txt")
+baseline_files = glob.glob("/scratch/public/txt_files/25background_experiment/*.txt")
 
 img_files = [x for x in baseline_files if "img" in x]
 
 for img in img_files:
+    with open(img, "r") as f:
+        lines = f.read().split('\n')
+        lines.remove('')
+        line_num = len(lines)
     with open(img.replace(".txt", ".shapes"), "w") as f:
-        for i in range(0, 99):
+        for i in range(0, line_num-1):
             f.writelines("608 608\n")
         f.writelines("608 608")
