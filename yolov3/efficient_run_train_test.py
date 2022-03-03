7968f6f45647a8041e6aa11a930cd782a5165027
Frank Willard <fcw@pizer.egr.duke.edu>
2022-03-03 12:07:24 -0500

    Fixed out paths

--- yolov3/efficient_run_train_test.py
+++ yolov3/efficient_run_train_test.py
@@ -15,6 +15,7 @@ parser.add_argument('--out_path', default='/scratch/public/jitter/wt/experiment_
 #train_path- /scratch/public/txt_files/25background_experiment/
 parser.add_argument('--train_path', default='/scratch/public/jitter/wt/experiments/')
 parser.add_argument('--experiment', default='Optimal_Ratio')
+parser.add_argument('--experiment_name', default='Optimal_Ratio_2_CG')
 parser.add_argument('--val_path', default='/scratch/public/jitter/wt/experiments/Test/')
 parser.add_argument('--epochs', default='300')
 parser.add_argument('--device', default='1')
@@ -24,6 +25,7 @@ out_path = args.out_path
 train_path = args.train_path
 val_path = args.val_path
 experiment = args.experiment
+experiment_name = args.experiment_name
 
 domains = ["EM", "SW"]
 
@@ -42,8 +44,7 @@ def optimalRatioFilter(element):
 optimal_ratio_combos = list(filter(optimalRatioFilter, combinations))
 
 experiment_path = os.path.join(train_path, experiment + "/")
-experiment_out_path = os.path.join(out_path, experiment + "/")
-
+experiment_out_path = os.path.join(out_path, experiment_name + "/")
 
 datasets = []
 for combo in optimal_ratio_combos:
@@ -60,7 +61,7 @@ for combo in optimal_ratio_combos:
     else:
       dataset_string = """Dataset(img_txt=experiment_path+'Train_{src}_Test_{dst}_Images.txt',
                   lbl_txt=experiment_path+'Train_{src}_Test_{dst}_Labels.txt',
-                  out_dir=out_path+'t_{src}_v_{dst}_{i}/',
+                  out_dir=experiment_out_path+'t_{src}_v_{dst}_{i}/',
                   img_txt_val=val_path+'{dst}_Images.txt',
                   lbl_txt_val=val_path+'{dst}_Labels.txt')""".format(src=combo[0],dst=combo[1],i=i)
     datasets.append(eval(dataset_string))
