4182bd07c8c8f21a02be338efc40f225922493ea
Yuxi Long <yl708@dcc-login-03.oit.duke.edu>
2022-04-13 13:08:33 -0400

    started or 100 experiments

--- train_wrapper.py
+++ train_wrapper.py
@@ -5,9 +5,15 @@ import os
 
 # domains = ['EM', 'NE', 'NW', 'SW']
 
-batch_sizes = [1, 2, 3, 4, 5, 6, 7, 8]
+# batch_sizes = [1, 2, 3, 4, 5, 6, 7, 8]
 
-for batch_size in batch_sizes:
+experiment_names = ["Color_Equalize_Domain", "Color_Equalize_Domain_100", "Lower_Bound_100", "Optimal_Ratio_100_1", "Test_Color_Equalize_Domain"]
+
+# for batch_size in batch_sizes:
+for experiment in experiment_names:
+        # Create directory for output log file
+        os.system(f'mkdir -p /work/yl708/bass/repro_bass_files/jitter/wt/experiment_results/{experiment}/')
+
         # os.system(f'python train.py --source {s_domain} --target {t_domain}')
         s =f"""#!/bin/bash
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -17,8 +23,8 @@ for batch_size in batch_sizes:
 #SBATCH --time='7-0'
 #SBATCH --chdir='/work/yl708/bass/repro_bass/yolov3/'
 #SBATCH --mem=0
-#SBATCH --output=/work/yl708/bass/repro_bass_files/jitter/wt/experiment_results/Optimal_Ratio_{batch_size}/R-%x.%j.out
-#SBATCH --error=/work/yl708/bass/repro_bass_files/jitter/wt/experiment_results/Optimal_Ratio_{batch_size}/R-%x.%j.err
+#SBATCH --output=/work/yl708/bass/repro_bass_files/jitter/wt/experiment_results/{experiment}/R-%x.%j.out
+#SBATCH --error=/work/yl708/bass/repro_bass_files/jitter/wt/experiment_results/{experiment}/R-%x.%j.err
 
 source ~/.bashrc
 source ~/.bash_profile
@@ -34,7 +40,7 @@ conda activate torch
 
 """
 
-        s += f'python efficient_run_train_test.py --experiment Optimal_Ratio_{batch_size}_CG --experiment_name Optimal_Ratio_{batch_size} --device 0 --supplemental_batch_size {batch_size}\n'
+        s += f'python efficient_run_train_test.py --experiment {experiment} --experiment_name {experiment} --device 0 --supplemental_batch_size 1\n'
 
-        with open(f'training_scripts/mb_{batch_size}.sh', 'w') as script:
+        with open(f'training_scripts/mb_{experiment}.sh', 'w') as script:
             script.write(s)
