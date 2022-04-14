6007bd78c13d2bb5c5e1a79c9ebbf0aed31a7953
Yuxi Long <yl708@dcc-login-03.oit.duke.edu>
2022-04-14 17:02:57 -0400

    added command to fetch all csv files

--- scripts/map_retriever/run_map_retriever.py
+++ scripts/map_retriever/run_map_retriever.py
@@ -3,7 +3,7 @@ import os
 from retrieve_maps import retrieve
 
 #directory holding experimental outputs
-directory = "/scratch/public/jitter/wt/experiment_results/Optimal_Ratio_6/"
+directory = "/scratch/public/jitter/wt/experiment_results/Optimal_Ratio_100_8/"
 
 #Will likely stay some- specified in run save train test py
 version = "v2"
@@ -14,3 +14,9 @@ output_dir = directory ## outputting the results to wherever we ran the experime
 output_fname = "optimal_ratio_experiment.csv"
 
 retrieve(directory, version, output_dir, output_fname)
+
+# After retrieving, run the bash command
+# rsync -zarv --prune-empty-dirs --include="*/" --include="*.csv" --exclude="*" /scratch/public/jitter/wt/experiment_results/ /work/yl708/experiment_outputs
+# to copy over all the csv files
+# Source : https://stackoverflow.com/questions/11111562/rsync-copy-over-only-certain-types-of-files-using-include-option
+
